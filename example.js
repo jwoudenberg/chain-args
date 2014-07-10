5201988081bfe2295bc25f991eac0219fc0409c9
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2014-07-10 21:10:20 +0200

    First working version.

--- example.js
+++ example.js
@@ -1,24 +1,65 @@
-var chainArgs = require('.');
+// var chainArgs = require('./');
 
-var fullName = chainArgs()
-    .property('first')
-    .property('last')
-    .ender('end', function(result) {
-        console.log(result.foo + ' ' + result.bar);
-    });
+// var fullName = chainArgs()
+//     .property('first')
+//     .property('last')
+//     .ender('end', function(result) {
+//         return result.foo + ' ' + result.bar;
+//     });
 
-fullName
-    .first('Jasper')
-    .last('Woudenber')
-    .end();             //Jasper Woudenberg
+// var myName = fullName
+//             .first('Jasper')
+//             .last('Woudenber')
+//             .end();
 
-var div = chainArgs()
-    .property('numerator')
-    .property('denominator')
-    .nextTick(function(result) {
-        console.log(result.numerator / result.denominator);
-    });
+// console.log(myName);    //Jasper Woudenberg
 
-div
-    .numerator(6)
-    .denominator(2);    //3
+// var div = chainArgs()
+//     .property('numerator')
+//     .property('denominator')
+//     .nextTick(function(result) {
+//         console.log(result.numerator / result.denominator);
+//     });
+
+// div
+//     .numerator(6)
+//     .denominator(2);    //3
+
+// var sum = chainArgs()
+//     .list('number')
+//     .ender('end', function(result) {
+//         return result.reduce(function(acc, value) {
+//             return acc + value;
+//         }, 0);
+//     });
+
+// var total = sum()
+//                 .number(4)
+//                 .number(2)
+//                 .end();
+
+// console.log(total);     //6
+
+var createChain = require('./lib/createChain');
+
+var MyChain = createChain({
+    foo: 'property',
+    list: 'array',
+    end: 'callback'
+});
+
+new MyChain(console.log)
+    .foo('bar')
+    .list(1)
+    .list(2)
+    .end();
+
+var MyChain2 = createChain({
+    foo: 'property',
+    list: 'array'
+});
+
+new MyChain2(console.log)
+    .foo('bar')
+    .list(1)
+    .list(2);
